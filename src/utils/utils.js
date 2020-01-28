99bf12826f98901ee37923042c06628bc40da71b
Nithin Murali <nithin.murali@zohocorp.com>
2020-01-28 11:46:02 +0530

    added cache

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,13 @@
 
 const utils = (function(){
 
-	const makeApiCall = function(method, url, body){
+	const makeApiCall = function(method, url, body, headers){
 		return new Promise((resolve, reject) => {
 			let xhr = new XMLHttpRequest();
 			xhr.open(method, url, true);
-
+			Object.keys(headers).forEach(function(headerKey){
+				xhr.setRequestHeader(headerKey, headers[headerKey])
+			});
 			xhr.onload = function(res){
 				const headers = xhr.getAllResponseHeaders();
 				const headersArray = headers.trim().split(/[\r\n]+/);
@@ -33,7 +35,7 @@ const utils = (function(){
 
 	const loadNasaImage = function(date){
 		let url = `https://api.nasa.gov/planetary/apod?api_key=8tKXFJvk4bzxmNizdRyj62p8ouqTEIo4LCoJO7FP&date=${date}`;
-		return makeApiCall('GET', url, true);
+		return makeApiCall('GET', url, null, {'Cache-Control': 'max-age=3600'});
 	};
 
 	return {
